4bae0118bb79c98446e1791d6be5f8514ef7eace
Bighnesh Kumar Pati <bighneshpati@gmail.com>
2022-02-03 09:14:02 +0530

    added to the new T&T Lab

---
+++ Lab4/Smruti.py
@@ -0,0 +1,17 @@
+_dict = dict();
+
+# _dict = {
+#     {
+#         "name" : "Smruti",
+#         "college" : "GCEK"
+#     },
+#     {
+#         "name" : "Bighnesh",
+#         "college" : "KIIT"
+#     }
+# }
+newDict = {
+    1 : "Bix",
+    2 : "Nex"
+}
+print(newDict[2])
